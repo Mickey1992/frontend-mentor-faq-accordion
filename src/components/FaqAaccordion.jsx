8faff68859aaa8e5c7114186f1b7fb1568e64386
Mickey1992 <wl920425@gmail.com>
2024-08-05 16:06:03 +0900

    switch the icon and hide/show the answer when click the question

--- src/components/FaqAaccordion.jsx
+++ src/components/FaqAaccordion.jsx
@@ -1,18 +1,29 @@
 import plusIcon from "../assets/images/icon-plus.svg";
 import minusIcon from "../assets/images/icon-minus.svg";
 
+import { useState } from "react";
+
 export default function FaqAccordion({ question, answer }) {
+	const [isOpen, setIsOpen] = useState(false);
+
+	function handleIconClick() {
+		setIsOpen((prev) => !prev);
+	}
+
 	return (
-		<div className="faq-accordion">
-			<div className="question">
+		<div className={"faq-accordion"}>
+			<div className="question" onClick={handleIconClick}>
 				<p>{question} </p>
+
 				<img
 					className="icon"
-					src={plusIcon}
+					src={isOpen ? minusIcon : plusIcon}
 					alt="a plus icon which means you can click to see the question answer"
 				></img>
 			</div>
-			<p className="answer">{answer}</p>
+			<p className="answer" hidden={!isOpen}>
+				{answer}
+			</p>
 		</div>
 	);
 }
